f207ca81dad8e5952d7d3c4adf648b3e5165d235
Minh <minhnguyen47431@gmail.com>
2024-09-11 23:14:03 +0700

    Initial Commit

---
+++ SwiftUI-Bootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Nguyễn Minh on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Nguyễn Minh on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
